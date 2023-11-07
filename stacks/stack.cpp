813b90f08872bbf3e9babb7c034e16adaeef7e67
Nishant <ntih5565@gmail.com>
2023-11-07 09:48:06 +0530

    Boom

--- stacks/stack.cpp
+++ stacks/stack.cpp
@@ -1,59 +1,75 @@
 #include <iostream>
 using namespace std;
 
-class Stack
-{
-     public:
-     int *arr;
-     int size;
-     int top;
-     Stack(int n)
-     {
-          size = n;
-          arr = new int[size];
-          top = -1;
-     }
-     void push(int x)
-     {
-          if (top == size - 1)
-          {
-               cout << "Stack Overflow" << endl;
-               return;
-          }
-          top++;
-          arr[top] = x;
-     }
-     void pop()
-     {
-          if (top == -1)
-          {
-               cout << "Stack Underflow" << endl;
-               return;
-          }
-          top--;
-     }
-     int Top()
-     {
-          if (top == -1)
-          {
-               cout << "Stack is Empty" << endl;
-               return -1;
-          }
-          return arr[top];
-     }
-     bool empty()
-     {
-          return top == -1;
-     }
-
+class Node {
+public:
+     int data;
+     Node* next;
 };
 
+class Stack {
+private:
+     Node* top;
+public:
+     Stack() : top(NULL) {}
 
-int main(){
+     void push(int data) {
+          Node* temp = new Node();
+          if (!temp) {
+               cout << "\nHeap Overflow";
+               exit(1);
+          }
+          temp->data = data;
+          temp->next = top;
+          top = temp;
+     }
+
+     bool isEmpty() {
+          return top == NULL;
+     }
+
+     int peek() {
+          if (!isEmpty())
+               return top->data;
+          else
+               exit(1);
+     }
+
+     void pop() {
+          if (top == NULL) {
+               cout << "\nStack Underflow" << endl;
+               return;
+          }
+          Node* temp = top;
+          top = top->next;
+          temp->next = NULL;
+          delete temp;
+     }
+
+     void display() {
+          Node* temp;
+          if (top == NULL) {
+               cout << "\nStack Underflow";
+               exit(1);
+          }
+          else {
+               temp = top;
+               while (temp != NULL) {
+                    cout << temp->data << "-> ";
+                    temp = temp->next;
+               }
+          }
+     }
+};
+
+int main() {
+     Stack s;
+     s.push(10);
+     s.push(20);
+     s.push(30);
+     s.display();
+     cout << "\nTop element is " << s.peek() << endl;
+     s.pop();
+     s.display();
      return 0;
-     Stack st(3);
-     st.push(1);
-     st.push(1);
-     st.push(1);
-     st.push(1);
 }
