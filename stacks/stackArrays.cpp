8242921140eed29fe5bb8b5e4a79ae0119029760
Nishant <ntih5565@gmail.com>
2023-11-28 20:31:03 +0530

    stacks and queues

--- stacks/stackArrays.cpp
+++ stacks/stackArrays.cpp
@@ -4,65 +4,74 @@
 #include <iostream>
 using namespace std;
 
-class Node {
-public:
-     int data;
-     Node* next;
-};
 
 class Stack {
-public:
-     Node* top;
 
-     Stack() {
-          top = NULL;
+
+public:
+     int* arr;
+     int top;
+     int size;
+
+     
+
+     Stack(int size) {
+          this->size = size;
+          arr = new int[size];
+          top = -1;
      }
 
      void push(int value) {
-          Node* newNode = new Node();
-          if (!newNode) {
-               cout << "Heap overflow";
-               exit(1);
+          if( size-top>1){
+               top++;
+               arr[top] = value;
+               cout<< "\nValue pushed -> "<< value;
+               cout<< "\nCurrent Stack is: ";
+               print();
           }
-          newNode->data = value;
-          newNode->next = top;
-          top = newNode;
+          else{
+               cout << "\nStack Overflow !!! Current Stack is: ";
+               print();
+               
+          }
      }
 
      void pop() {
-          if (top == NULL) {
-               cout << "Underflow" << endl;
-               return;
-          }
-          Node* temp = top;
-          top = top->next;
-          temp->next = NULL;
-          delete temp;
+          top--;
      }
 
      int peek() {
-          if (!empty())
-               return top->data;
-          else
-               exit(1);
+          return arr[top];
      }
 
-
      bool empty() {
-          return top == NULL;
+          if(top>=0){
+               return false;
+          }else{
+               return true;
+          }
      }
 
      void print() {
-          Node* temp = top;
-          while (temp != NULL) {
-               cout << temp->data << "-> ";
-               temp = temp->next;
+          if ( empty()){
+               cout << "Stack is empty";
+               return;
           }
+          else{
+               for ( int i = 0; i<= top; i++){
+               cout << arr[i] << " ";
+          }
+          cout << endl;
+          }
+
      }
 };
 
 int main() {
-     Stack st;
+     cout << "Enter size of the Stack: ";
+     int size;
+     cin>>size;
+     Stack st(size);
      int choice, value;
      do {
           cout << "\n1. Push\n2. Pop\n3. Peek\n4. Check if Empty\n5. Print Stack\n6. Exit\n";
@@ -84,7 +93,8 @@ int main() {
                               cout << "stack is not empty ";
                          }
                          break;
-               case 5: st.print();
+               case 5:   cout<< "\nStack is: ";
+                         st.print();
                          break;
                case 6: cout << "Exiting...";
                          break;
